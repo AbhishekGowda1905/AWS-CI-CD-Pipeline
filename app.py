3739cd606bc322ea7d9af71c9705f3bd53b3286b
AbhishekGowda_19_05 <72699674+AbhishekGowda1905@users.noreply.github.com>
2024-07-13 16:10:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ app=application
 
 ## Route for a home page
 
+
+
 @app.route('/')
 def index():
     return render_template('index.html') 
@@ -40,5 +42,3 @@ def predict_datapoint():
 if __name__=="__main__":
     # app.run(host="0.0.0.0",port=8080)        
     app.run(host='0.0.0.0', port=8080)        
-
-
